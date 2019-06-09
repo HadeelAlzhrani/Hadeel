4ccb23c2648a1638c79fcb565ea8c3eca5104a08
HadeelAlzhrani <htsalzhrani@gmail.com>
2019-06-09 10:29:42 +0300

    views and urls

--- Account/models.py
+++ Account/models.py
@@ -2,26 +2,32 @@ from django.db import models
 
 # Create your models here.
 class User (models.Model):
-    user_name=models.CharField(max_length= None)
+    user_name=models.CharField(max_length=20)
+    first_name= models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
     email=models.EmailField (null=True)
-    passwrd= models.CharField(max_length=20)
     registration_date= models.DateField
     date_of_birth= models.DateField(blank=True, null=True)
-    gender= (
-        ('Male', 'Male'),
-        ('Female', 'Female'),
-        ('Other', 'Other'),
+    gender_choices = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+        ('O', 'Other')
     )
-    avatar_image=models.ImageField (blank==True, null=True, upload_to="avatar/%Y/%m/%D/")
+    gender = models.CharField(
+        max_length=1,
+        choices=gender_choices,
+    )
+    avatar_image=models.ImageField (blank=True, null=True, upload_to="avatar/%Y/%m/%D/")
 
 class Wish (models.Model):
-    user_name = models.CharField
-    wish= models.CharField(max_length= 140)
-    date_and_time= models.DateField(blank=True, null=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    description= models.CharField(max_length= 140)
+    published_date= models.DateField(auto_now_add=True)
 
 class Comment (models.Model):
-    user_name= models.CharField
-    comment_text= models.CharField
-    comment_date= models.DateField(blank=True, null=True)
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    wish= models.ForeignKey(Wish, on_delete=models.CASCADE)
+    comment_text= models.CharField(max_length=140)
+    comment_date= models.DateTimeField(blank=True, null=True)
 
 
--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_page(request):
+    return HttpResponse ("<h1> Omniat </h1>")
+
+def profile(request):
+    return HttpResponse
